e785b908beefe0f031c37448f9ab53e5d167a556
Max Crisafulli <33032859+MaxCrisafulli@users.noreply.github.com>
2020-02-08 16:36:48 -0800

    Add files via upload

---
+++ Sat 2-8-2020 Test/transmitter2.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN         
+const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+int pot_pin = A2;
+int potval = 0;
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();                  //Starting the Wireless communication
+  radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening();          //This sets the module as transmitter
+}
+
+void loop()
+{
+  potval = analogRead(pot_pin);
+  Serial.println(potval);
+  radio.write(&potval, sizeof(potval));  //Sending the message to receiver 
+  delay(500);
+ }
