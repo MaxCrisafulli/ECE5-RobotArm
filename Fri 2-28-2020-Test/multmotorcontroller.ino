553e6a513a32cef29d186d50ba14f9080748d3ff
Max Crisafulli <33032859+MaxCrisafulli@users.noreply.github.com>
2020-02-25 14:09:58 -0800

    Add files via upload

--- Fri 2-28-2020-Test/multmotorcontroller.ino
+++ Fri 2-28-2020-Test/multmotorcontroller.ino
@@ -1,22 +1,40 @@
+//MAKE SURE TO SET POWER SUPPLY FOR EACH MOTOR AT 12V, 2AMP
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "00001";
 
+//TIME VARIABLES
 const unsigned long T_rec = 5;
 long next_rec = 0;
 const unsigned long T_spin = 1000;
 long next_spin = 0;
 
-const int dirPin = 2;  // Direction
-const int stepPin = 3; // Step
-int pos  =  0;
-int curpos = 0;
+
+//MOTOR PINS
+const int dirPin1 = 2;  // Direction
+const int stepPin1 = 3; // Step
+const int dirPin2 = 4;  
+const int stepPin2 = 5; 
+const int dirPi3 = 6;  
+const int stepPin3 = 7; 
+
+//POSITION VARIABLES
+int pos1  =  0;
+int curpos1 = 0;
+int pos2  =  0;
+int curpos2 = 0;
+int pos3  =  0;
+int curpos3 = 0;
  
 void setup() {
-  pinMode(stepPin,OUTPUT); 
-  pinMode(dirPin,OUTPUT);
+  pinMode(stepPin1,OUTPUT); 
+  pinMode(dirPin1,OUTPUT);
+  pinMode(stepPin2,OUTPUT); 
+  pinMode(dirPin2,OUTPUT);
+  pinMode(stepPin3,OUTPUT); 
+  pinMode(dirPin3,OUTPUT);
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
@@ -25,50 +43,115 @@ void setup() {
 }
 
 void loop() {
+
+  //RF RECEIVER CODE
   if (millis() >= next_rec) {
     if (radio.available())              //Looking for the data.
     {
-      int val;                          //Saving the incoming data
-      radio.read(&val, sizeof(val));    //Reading the data
-      pos = val/10;
+      int val1;                         //Saving the incoming data
+      int val2;
+      int val3;   
+      radio.read(&val1, sizeof(val1));    //Reading the data
+      radio.read(&val2, sizeof(val2));    //Reading the data
+      radio.read(&val3, sizeof(val3));    //Reading the data
+      pos1 = val1/10;
+      pos2 = val2/10;
+      pos3 = val3/10;
     }
     next_rec += T_rec;
   }
 
-
- 
-  if (curpos != pos) {
+  //MOTOR 1 LOOP 
+  if (curpos1 != pos1) {
+    //LOCAL LOOP VARIABLES
     int DIFF_STEPS = 0;
-    int DELTASTEP = pos-curpos;
+    int DELTASTEP = pos1-curpos1;
     DIFF_STEPS = 2 * abs(DELTASTEP);
-    
-    Serial.println(curpos);
-    Serial.println(pos);
-    Serial.println(DIFF_STEPS);
-    Serial.println(DELTASTEP);
-    Serial.println("");
-    
-    if (curpos > pos) {
-      digitalWrite(dirPin,HIGH);
-      Serial.println("Forward");
+
+    //DIRECTION CHECKS
+    if (curpos1 > pos1) {
+      digitalWrite(dirPin1,HIGH);
+      //SETS DIRECTION AS FORWARD
     }
     
-    else if (curpos < pos){
-      digitalWrite(dirPin,LOW);
-      Serial.println("Back");
+    else if (curpos1 < pos1){
+      digitalWrite(dirPin1,LOW);
+      //SETS DIRECTION AS BACK
     }
-    
+
+    //MOVEMENT COMMANDS
     for (int x = 0; x < DIFF_STEPS; x++) {
-      digitalWrite(stepPin,HIGH);
+      digitalWrite(stepPin1,HIGH);
       delay(10);  
-      digitalWrite(stepPin,LOW);
+      digitalWrite(stepPin1,LOW);
       delay(10);
-      curpos += 1;
+      curpos1 += 1;
     }
     
-    curpos = pos;
+    curpos1 = pos1;
     DIFF_STEPS = 0;
   }
-}
 
-//set at 12V 2Amp
+  //MOTOR 2 LOOP 
+  if (curpos2 != pos2) {
+    //LOCAL LOOP VARIABLES
+    int DIFF_STEPS = 0;
+    int DELTASTEP = pos2-curpos2;
+    DIFF_STEPS = 2 * abs(DELTASTEP);
+
+    //DIRECTION CHECKS
+    if (curpos2 > pos2) {
+      digitalWrite(dirPin2,HIGH);
+      //SETS DIRECTION AS FORWARD
+    }
+    
+    else if (curpos2 < pos2){
+      digitalWrite(dirPin2,LOW);
+      //SETS DIRECTION AS BACK
+    }
+
+    //MOVEMENT COMMANDS
+    for (int x = 0; x < DIFF_STEPS; x++) {
+      digitalWrite(stepPin2,HIGH);
+      delay(10);  
+      digitalWrite(stepPin2,LOW);
+      delay(10);
+      curpos2 += 1;
+    }
+    
+    curpos2 = pos2;
+    DIFF_STEPS = 0;
+  }
+
+  //MOTOR 3 LOOP 
+  if (curpos3 != pos3) {
+    //LOCAL LOOP VARIABLES
+    int DIFF_STEPS = 0;
+    int DELTASTEP = pos3-curpos3;
+    DIFF_STEPS = 2 * abs(DELTASTEP);
+
+    //DIRECTION CHECKS
+    if (curpos3 > pos3) {
+      digitalWrite(dirPin3,HIGH);
+      //SETS DIRECTION AS FORWARD
+    }
+    
+    else if (curpos3 < pos3){
+      digitalWrite(dirPin3,LOW);
+      //SETS DIRECTION AS BACK
+    }
+
+    //MOVEMENT COMMANDS
+    for (int x = 0; x < DIFF_STEPS; x++) {
+      digitalWrite(stepPin3,HIGH);
+      delay(10);  
+      digitalWrite(stepPin3,LOW);
+      delay(10);
+      curpos3 += 1;
+    }
+    
+    curpos3 = pos3;
+    DIFF_STEPS = 0;
+  }
+  
+}
--- Fri 2-28-2020-Test/multtransmitter2.ino
+++ Fri 2-28-2020-Test/multtransmitter2.ino
@@ -3,14 +3,18 @@
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN         
 const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
-int pot_pin = A2;
-int potval = 0;
+
+//POTENTIOMETER PINS
+int pot_pin1 = A2;
+int potval1 = 0;
+int pot_pin2 = A3;
+int potval2 = 0;
+int pot_pin3 = A4;
+int potval3 = 0;
+
+//TIME VARIABLES
 const unsigned long T_trans = 10;
 long next_trans = 0;
-/*
-const unsigned long T_blink = 500;
-long next_blink = 0;
-*/
 
 void setup() {
   Serial.begin(9600);
@@ -24,19 +28,14 @@ void setup() {
 
 void loop()
 {
+  //RF TRANSMITTER CODE
   if (millis() >= next_trans) {
-    potval = analogRead(pot_pin);
-    Serial.println(potval);
-    radio.write(&potval, sizeof(potval));
+    potval1 = analogRead(pot_pin1);
+    potval2 = analogRead(pot_pin2);
+    potval3 = analogRead(pot_pin3);
+    radio.write(&potval1, sizeof(potval1));
+    radio.write(&potval2, sizeof(potval2));
+    radio.write(&potval3, sizeof(potval3));
     next_trans += T_trans;
   }
-  /*
-  if (millis() >= next_blink) {
-     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-     delay(100);                        // wait for half a second
-     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-     next_blink += T_blink;
-  }
-  */
-  
  }
